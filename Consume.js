0c8e1e7c8fbf23b947481e14073987d014793356
aritro66 <ghosharitro66@gmail.com>
2022-05-06 17:46:27 +0530

    imporoved more

--- Consume.js
+++ Consume.js
@@ -5,7 +5,7 @@ const QUEUE_URL = `https://sqs.us-east-1.amazonaws.com/${process.env.ACCOUNT_ID}
 
 
 var messageCount = 0;
-module.exports.consumer = async (event) => {
+module.exports.consumer =  (event) => {
 
   try {
     const random = Math.random();
@@ -24,27 +24,22 @@ module.exports.consumer = async (event) => {
   } catch (err) {
     console.log("error occured");
     console.log(JSON.stringify(event));
-    event.Records.forEach(ele => {
+    event.Records.forEach(async(ele) => {
       const retries = ele.attributes.ApproximateReceiveCount;
       const receipt = ele.receiptHandle;
       console.log(retries);
-      if (retries <= 3) {
-        var Visibilityparams = {
+      if (retries > 3) {
+        return;
+      }
+      console.log("w");
+      const Visibilityparams = {
           QueueUrl: QUEUE_URL,
           ReceiptHandle: receipt,
           VisibilityTimeout: parseInt(Backoff(retries))
         };
-        sqs.changeMessageVisibility(Visibilityparams, function (err, data) {
-          console.log("visible");
-          if (err) {
-            console.log(err, err.stack);
-          }
-          else {
-            console.log(data);
-            console.log("v");
-          }
-        });
-      }
+        console.log(Visibilityparams)
+        const chkdata = await sqs.changeMessageVisibility(Visibilityparams).promise();
+        console.log(`checking data : ${chkdata}`);
     });
 
 
