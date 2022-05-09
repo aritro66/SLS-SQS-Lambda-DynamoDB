28ab089a933f8175f2b6e3db849dd6777a2df942
aritro66 <ghosharitro66@gmail.com>
2022-05-09 11:42:11 +0530

    dynamodb applied

--- Consume.js
+++ Consume.js
@@ -1,24 +1,28 @@
-const { SQS } = require("aws-sdk");
-
-const sqs = new SQS();
+const AWS = require("aws-sdk");
+const uuid = require('uuid');
+const sqs = new AWS.SQS();
 const QUEUE_URL = `https://sqs.us-east-1.amazonaws.com/${process.env.ACCOUNT_ID}/demo`;
-const QUEUE_URL2 = `https://sqs.us-east-1.amazonaws.com/${process.env.ACCOUNT_ID}/demodlq`;
-
+// const QUEUE_URL2 = `https://sqs.us-east-1.amazonaws.com/${process.env.ACCOUNT_ID}/demodlq`;
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 var messageCount = 0;
 module.exports.consumer = async (event) => {
 
 
-  const random = Math.random();
+  const random = Math.random(); // random no. for error generation
 
   if (event.Records) {
     messageCount += event.Records.length
   }
   console.log('Message Count: ', messageCount)
+
+  // dlqlist messages whose recieve count is more than 3
+  // visibility messages whose recieve count is less than equal 3
   const dlqlist = event.Records.filter(ele => ele.attributes.ApproximateReceiveCount > 3);
   const visibilitylist = event.Records.filter(ele => ele.attributes.ApproximateReceiveCount <= 3);
   console.log("dlq", JSON.stringify(dlqlist));
   console.log("visibility", JSON.stringify(visibilitylist));
+
   
   for (let i = 0; i < dlqlist.length; i++) {
     const retries = dlqlist[i].attributes.ApproximateReceiveCount;
@@ -27,19 +31,23 @@ module.exports.consumer = async (event) => {
 
     console.log("wu");
     const params = {
-      QueueUrl: QUEUE_URL2,
-      MessageBody: dlqlist[i].body
+      TableName: process.env.DYNAMODB_DLQ_TABLE,
+      Item: {
+        id: uuid.v1(),
+        Value: dlqlist[i].body,
+        updatedAt: new Date().getTime()
+      }
     };
     console.log(params);
-    await sqs.sendMessage(params).promise();
+    await dynamoDb.put(params).promise(); // save in database
     const deleteparams = {
       QueueUrl: QUEUE_URL,
       ReceiptHandle: receipt,
     }
-    await sqs.deleteMessage(deleteparams).promise();
+    await sqs.deleteMessage(deleteparams).promise(); // deleting message from sqs
   }
 
-  if (random <= 0.5 || visibilitylist.length==0) {
+  if (random <= 0.5 || visibilitylist.length == 0) {  // no error criteria
     return;
   }
 
@@ -55,10 +63,10 @@ module.exports.consumer = async (event) => {
       VisibilityTimeout: parseInt(Backoff(retries))
     };
     console.log(Visibilityparams)
-    await sqs.changeMessageVisibility(Visibilityparams).promise();
+    await sqs.changeMessageVisibility(Visibilityparams).promise();  // changing message visibility
   }
 
-  throw new Error(`Im an error!`)
+  throw new Error(`Im an error!`) // emiting error
 
 }
 
