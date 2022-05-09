b367f92f0b32c356712c2328cdf54cbe94da610c
aritro66 <ghosharitro66@gmail.com>
2022-05-09 08:42:22 +0530

    update

--- Consume.js
+++ Consume.js
@@ -2,6 +2,7 @@ const { SQS } = require("aws-sdk");
 
 const sqs = new SQS();
 const QUEUE_URL = `https://sqs.us-east-1.amazonaws.com/${process.env.ACCOUNT_ID}/demo`;
+const QUEUE_URL2 = `https://sqs.us-east-1.amazonaws.com/${process.env.ACCOUNT_ID}/demodlq`;
 
 
 var messageCount = 0;
@@ -14,12 +15,35 @@ module.exports.consumer = async (event) => {
     messageCount += event.Records.length
   }  
   console.log('Message Count: ', messageCount)
-  console.log(JSON.stringify(event))
-  if (event.Records[0].attributes.ApproximateReceiveCount > 3) {
-    throw new Error("more than 3 tries");
+  const dlqlist=event.Records.filter(ele=>ele.attributes.ApproximateReceiveCount>3);
+  const visibilitylist=event.Records.filter(ele=>ele.attributes.ApproximateReceiveCount<=3);
+  console.log("dlq",JSON.stringify(dlqlist));
+  console.log("visibility",JSON.stringify(visibilitylist));
+  // if (event.Records[0].attributes.ApproximateReceiveCount > 3) {
+  //   throw new Error("more than 3 tries");
+  // }
+  for(let i=0;i<dlqlist.length;i++) 
+  {
+    const retries = dlqlist[i].attributes.ApproximateReceiveCount;
+    const receipt = dlqlist[i].receiptHandle;
+    console.log(retries);
+    
+    console.log("wu");
+    const params = {
+        QueueUrl: QUEUE_URL2,
+        MessageBody:dlqlist[i].body
+      };
+      console.log(params);
+     await sqs.sendMessage(params).promise();
+    const deleteparams={
+      QueueUrl: QUEUE_URL,
+      ReceiptHandle: receipt,
+    }
+    await sqs.deleteMessage(deleteparams).promise();
   }
+
   if (random > 0.5) {
-    event.Records.forEach((ele) => {
+    visibilitylist.forEach((ele) => {
       const retries = ele.attributes.ApproximateReceiveCount;
       const receipt = ele.receiptHandle;
       console.log(retries);
